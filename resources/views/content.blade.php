c2f54ebdbf2763a15cfd5441441694a6cbae427c
Esme Meraz <2016030013@gmail.com>
2019-06-24 20:48:57 -0600

    plantilla

---
+++ resources/views/content.blade.php
@@ -0,0 +1,26 @@
+@extends('footer')
+@extends('breadcrumbs')
+@extends('sidebar')
+@extends('header')	
+
+	<div id="main-wrapper">
+		@section('header')
+		@parent
+		@section('sidebar')	
+		@parent
+		<div class="page-wrapper">
+			<div class="container-fluid">
+				@section('breadcrumbs')	
+				@parent
+				<div class="card">
+					<h1>jala</h1>
+				</div>
+			</div>
+		</div>
+		@section('footer')
+		@parent
+	</div>
+@endsection
+@endsection
+@endsection
+@endsection
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,23 @@
+@section('footer')
+	<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{asset('plugins/bootstrap/js/popper.min.js')}}"></script>
+    <script src="{{asset('plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{asset('js/perfect-scrollbar.jquery.min.js')}}"></script>
+    <!--Wave Effects -->
+    <script src="{{asset('js/waves.js')}}"></script>
+    <!--Menu sidebar -->
+    <script src="{{asset('js/sidebarmenu.js')}}"></script>
+    <!--stickey kit -->
+    <script src="{{asset('plugins/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
+    <script src="{{asset('plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{asset('js/custom.min.js')}}"></script>
+    <!-- ============================================================== -->
+    <!-- Style switcher -->
+    <!-- ============================================================== -->
+    <script src="{{asset('plugins/styleswitcher/jQuery.style.switcher.js')}}"></script>
+</body>
+</html>
+@show
